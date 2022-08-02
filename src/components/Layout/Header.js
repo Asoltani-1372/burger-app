8a0562fa46f5b2406faef2239eda8038856e1bd0
a.soltani <soltaniali.1994@gmail.com>
2022-08-02 23:38:16 +0430

    2 commit

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,19 +2,21 @@ import {Fragment} from 'react'
 
 import mealsimage from '../../assets/meals.jpg'
 import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
 
-const Header = props => {
-  return 
+const Header = (props) => {
+  return (
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <button>cart</button>
+                <HeaderCartButton />
             </header>
-            <div classname={['main-image']}>
-                <img src={mealsimage}/>
+            <div className={classes['main-image']}>
+                <img src={mealsimage} alt="a tavle full of delicious food!"/>
             </div>
 
         </Fragment>
+    )    
 }
 
 export default Header
